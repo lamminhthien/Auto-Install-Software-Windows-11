0fe2eb7914ced19398f40f4b7af83ab01d205858
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-02-24 00:48:17 -0800

    Add files via upload

---
+++ install_vscode.ps1
@@ -0,0 +1,15 @@
+﻿# This script downloads and installs Visual Studio Code.
+
+function Install-VisualStudioCode {
+    Write-Host "Downloading Visual Studio Code..."
+    $Path = $env:TEMP
+    $Installer = "VSCodeUserSetup-x64.exe"
+    $URL = "https://aka.ms/win32-x64-user-stable"
+    Invoke-WebRequest $URL -OutFile $Path\$Installer
+
+    Write-Host "Installing Visual Studio Code..."
+    Start-Process -FilePath $Path\$Installer -Args "/silent /mergetasks=!runcode" -Verb RunAs -Wait
+    Remove-Item $Path\$Installer
+}
+
+Install-VisualStudioCode
