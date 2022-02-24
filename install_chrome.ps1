98fe6327c05410aab720380c446bebc4e278bb68
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-02-24 00:30:38 -0800

    Add files via upload

---
+++ install_chrome.ps1
@@ -0,0 +1,16 @@
+﻿# This script downloads and installs Google Chrome.
+
+function Install-GoogleChrome()
+{
+    Write-Host "Downloading Google Chrome..."
+    $Path = $env:TEMP
+    $Installer = "chrome_installer.exe"
+    $URL = "http://dl.google.com/chrome/install/375.126/chrome_installer.exe"
+    Invoke-WebRequest $URL -OutFile $Path\$Installer
+
+    Write-Host "Installing Google Chrome..."
+    Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait
+    Remove-Item $Path\$Installer
+}
+
+Install-GoogleChrome
