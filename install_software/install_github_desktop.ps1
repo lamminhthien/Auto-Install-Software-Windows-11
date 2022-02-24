2fea92ad2a9f4bc8001b90bc71d44ec662ee1efa
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-02-24 03:25:38 -0800

    Github desktop

---
+++ install_software/install_github_desktop.ps1
@@ -0,0 +1,7 @@
+# Install Chocolatey
+if (!(Get-Command choco.exe -ErrorAction SilentlyContinue)) {
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))}
+
+# Install Github Desktop
+
+choco install github-desktop
