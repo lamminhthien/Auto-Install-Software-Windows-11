ac2ee02939ea96ab0492078ac3bc0965418c66ac
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-02-25 08:16:47 -0800

    change to winget command

--- install_software/install_sqlServerManagementStudio.ps1
+++ install_software/install_sqlServerManagementStudio.ps1
@@ -1,15 +1,1 @@
-﻿# This script installs SQL Server Management Studio.
-
-function Install-SQLServerManagementStudio {
-    Write-Host "Downloading SQL Server Management Studio..."
-    $Path = $env:TEMP
-    $Installer = "SSMS-Setup-ENU.exe"
-    $URL = "https://aka.ms/ssmsfullsetup"
-    Invoke-WebRequest $URL -OutFile $Path\$Installer
-
-    Write-Host "Installing SQL Server Management Studio..."
-    Start-Process -FilePath $Path\$Installer -Args "/install /quiet" -Verb RunAs -Wait
-    Remove-Item $Path\$Installer
-}
-
-Install-SQLServerManagementStudio
+﻿winget install  Microsoft.SQLServerManagementStudio
