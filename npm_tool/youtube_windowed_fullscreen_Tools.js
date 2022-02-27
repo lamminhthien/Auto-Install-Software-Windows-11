f40ab5a28ab0f9baedd1f9db7538e229b04f1268
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-02-27 18:09:58 +0700

    Auto search youtube-video "Helo" , when keyword empty

--- npm_tool/youtube_windowed_fullscreen_Tools.js
+++ npm_tool/youtube_windowed_fullscreen_Tools.js
@@ -8,11 +8,12 @@ prompt.start();
 
 // Give text to yt-search
 prompt.get(["keyword"], function (err, result) {
-
   // Search and print list of video for user to choose
   const runSearch = async () => {
-    const listVideo = await yts(result.keyword);
-
+    var listVideo;
+    result.keyword == null
+      ? (listVideo = await yts(result.keyword))
+      : (listVideo = await yts("hello"));
     // How many result do you want to see
     prompt.get(["count"], function (err, result) {
       var videos = listVideo.videos.slice(0, result.count);
@@ -21,18 +22,22 @@ prompt.get(["keyword"], function (err, result) {
       videos.length == 0
         ? (videos = listVideo.videos.slice(0, 3))
         : (videos = listVideo.videos.slice(0, result.count));
-        // Print list for you choose video to play as embbed mode
-      videos.forEach((v,index) => {
-         console.log(`((${index+1}))<<< ${v.title} (${v.timestamp}) | ${v.author.name} |>>>>`);
-        
+      // Print list for you choose video to play as embbed mode
+      videos.forEach((v, index) => {
+        console.log(
+          `((${index + 1}))<<< ${v.title} (${v.timestamp}) | ${
+            v.author.name
+          } |>>>>`
+        );
+
         // console.log(videos)
       });
 
       // Select video you want to play
-      prompt.get(["number"],function(err,result){
-        const embed = youtubeEmbed(videos[result.number-1].url)
-        console.log(`Now openning ${videos[(result.number)-1].title} `);
-        open(`https:${embed}`)
+      prompt.get(["number"], function (err, result) {
+        const embed = youtubeEmbed(videos[result.number - 1].url);
+        console.log(`Now openning ${videos[result.number - 1].title} `);
+        open(`https:${embed}`);
       });
     });
   };
