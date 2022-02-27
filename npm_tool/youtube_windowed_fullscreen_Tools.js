35981908f10a41f0bac89d41af25ba26456aa76d
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-02-27 17:58:34 +0700

    fix position video index select

--- npm_tool/youtube_windowed_fullscreen_Tools.js
+++ npm_tool/youtube_windowed_fullscreen_Tools.js
@@ -22,15 +22,18 @@ prompt.get(["keyword"], function (err, result) {
         ? (videos = listVideo.videos.slice(0, 3))
         : (videos = listVideo.videos.slice(0, result.count));
         // Print list for you choose video to play as embbed mode
-      videos.forEach(function (v) {
-        console.log(`<<<${v.title} (${v.timestamp}) | ${v.author.name} |>>>>`);
-        console.log(videos)
+      videos.forEach((v,index) => {
+         console.log(`((${index+1}))<<< ${v.title} (${v.timestamp}) | ${v.author.name} |>>>>`);
+        
+        // console.log(videos)
       });
 
-    //   // Select video you want to play
-    //   prompt.get(["number"],function(err,result){
-    //     const embed = youtubeEmbed(videos[result.number].)
-    //   });
+      // Select video you want to play
+      prompt.get(["number"],function(err,result){
+        const embed = youtubeEmbed(videos[result.number-1].url)
+        console.log(`Now openning ${videos[(result.number)-1].title} `);
+        open(`https:${embed}`)
+      });
     });
   };
 
