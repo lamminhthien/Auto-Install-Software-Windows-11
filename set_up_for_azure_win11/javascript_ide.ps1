eef1b86be9326b37a5d0820555d4615e1d53126c
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-03-07 08:16:22 +0700

    Update javascript_ide.ps1

--- set_up_for_azure_win11/javascript_ide.ps1
+++ set_up_for_azure_win11/javascript_ide.ps1
@@ -1,12 +1,12 @@
 # VPS i use have pre-installed chocolatey
 # Git
-choco install git
+choco install git -y
 # Github desktop
 choco install github-desktop -y
 # Visual studio code
-choco install vscode
+choco install vscode -y
 # NodeJS NPM
-choco install nodejs
+choco install nodejs -y
 
 
 # Install TSLint Global
