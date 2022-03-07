7a10216bb721bbea2bd0c2f0e238ba3626e178d0
lamminhthien <99172799+lamminhthien@users.noreply.github.com>
2022-03-07 07:52:13 +0700

    Add Script for vps

--- install_software/install_github_desktop.ps1
+++ install_software/install_github_desktop.ps1
@@ -1,1 +1,7 @@
-winget install GitHub.GitHubDesktop
+# Install Chocolatey
+if (!(Get-Command choco.exe -ErrorAction SilentlyContinue)) {
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))}
+
+# Install Github Desktop
+
+choco install github-desktop -y
---
+++ set_up_for_azure_win11/javascript_ide.ps1
@@ -0,0 +1,14 @@
+# VPS i use have pre-installed chocolatey
+# Github desktop
+choco install github-desktop -y
+# Visual studio code
+choco install vscode
+# NodeJS NPM
+choco install nodejs
+
+
+# Install TSLint Global
+npm install -g tslint
+# Install TSLink Vscode
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+
